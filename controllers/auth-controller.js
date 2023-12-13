c78cb5ece327adc03898842a5a1c2730114f735c
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2023-12-13 12:07:30 +0200

    update 4 hw userauth

---
+++ controllers/auth-controller.js
@@ -0,0 +1,15 @@
+import User from "../models/User.js";
+import { HttpError } from "../helpers/index.js";
+import { ctrlWrapper } from "../decorators/index.js";
+
+const signup = async (req, res, next) => {
+  const newUser = await User.create(req.body);
+  res.status(201).json({
+    username: newUser.username,
+    email: newUser.email,
+  });
+};
+
+export default {
+  signup: ctrlWrapper(signup),
+};
---
+++ routes/api/auth-router.js
@@ -0,0 +1,15 @@
+import express from "express";
+import authController from "../../controllers/auth-controller.js";
+import { isEmptyBody, isValidId } from "../../middlewares/index.js";
+import { validateBody } from "../../decorators/index.js";
+import { userSigninSchema, userSignupSchema } from "../../models/User.js";
+const authRouter = express.Router();
+
+authRouter.post(
+  "/signup",
+  isEmptyBody,
+  validateBody(userSignupSchema),
+  authController.signup
+);
+
+export default authRouter;
