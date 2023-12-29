5a6dab7231f3d0a01d5573b9135bc0beb1725063
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2023-12-29 13:07:31 +0200

    Revert "email added"
    This reverts commit 6c55bf2f94c484d1df58dfe8b36ac6c3a2d91be5.

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -2,11 +2,7 @@ import express from "express";
 import authController from "../../controllers/auth-controller.js";
 import { isEmptyBody, authenticate } from "../../middlewares/index.js";
 import { validateBody } from "../../decorators/index.js";
-import {
-  userSigninSchema,
-  userSignupSchema,
-  userEmailSchema,
-} from "../../models/User.js";
+import { userSigninSchema, userSignupSchema } from "../../models/User.js";
 const authRouter = express.Router();
 
 authRouter.post(
@@ -26,12 +22,4 @@ authRouter.get("/current", authenticate, authController.getCurrent);
 
 authRouter.post("/signout", authenticate, authController.signout);
 
-//*EMAIL
-authRouter.get("/verify/:verificationCode", authController.verify);
-authRouter.post(
-  "/verify",
-  isEmptyBody,
-  validateBody(userEmailSchema),
-  authController.resendVerify
-);
 export default authRouter;
